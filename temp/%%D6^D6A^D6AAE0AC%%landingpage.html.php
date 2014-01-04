7b81d5d663e429dc54e167a27ef58666d2223a9d
junayeed <junayeed@gmail.com>
2014-01-04 14:36:00 +0600

    User Registration

---
+++ temp/%%D6^D6A^D6AAE0AC%%landingpage.html.php
@@ -0,0 +1,59 @@
+<?php /* Smarty version 2.6.6, created on 2014-01-04 09:01:20
+         compiled from E:/xampp/htdocs/stf/app_contents/standard/registration/landingpage.html */ ?>
+<html>
+    <head>
+        <title>User Registration</title><?php if ($this->_tpl_vars['SYSTEM_PRODUCTION_MODE'] != 'Yes'): ?><!-- ** THIS NOTE ONLY SHOWS IN DEVELOPMENT MODE *** Purpose: This template is used to show the login form Javascript requirements: In addition to standard javascripts such as evoknow.js, messages.js you will need to create <?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/<?php echo $this->_tpl_vars['SYSTEM_APP_PREFIX']; ?>
+/<?php echo $this->_tpl_vars['SYSTEM_APP_PREFIX']; ?>
+.js to store any custom javascript functions needed for this form.--><?php endif; ?>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <!-- load javascript error/success messages for javascripts -->
+        <script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/messages.js"></script>
+        <!-- load standard javascript library -->
+        <script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/common.js"></script>
+        <!-- load current application specific javascript -->
+        <script language="JavaScript" src="<?php echo $this->_tpl_vars['REL_TEMPLATE_DIR']; ?>
+/<?php echo $this->_tpl_vars['SYSTEM_APP_PREFIX']; ?>
+.js"></script>
+        <script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/jquery.js"></script>
+        <!-- setup default info -->
+        <link rel="stylesheet" href="/app_contents/common/css/login.css" type="text/css">
+    </head>
+    <body>
+        <div class="registration">
+            <h1>Sweden Bangladesh Trust Fund Registration Form</h1>
+            <form name="registrationForm"  action="<?php echo $this->_tpl_vars['SCRIPT_NAME']; ?>
+" method="POST">
+                <div id="rigistrationContent">
+                    <?php if ($this->_tpl_vars['msg'] == 'Success'): ?>
+                      <div class="success">
+                        <h3> Congratulations! </h3>
+                        <p>
+                            You have successfully registered for Sweden Bangladesh Trust Fund. Please login and submit your application.
+                        </p>
+                      </div>  
+                        <p class="submit">
+                            <input type="button" name="commit" value="Go to Login" onclick="location.href='/app/standard/login/login.php'" >
+                        </p>
+                        
+                    <?php else: ?>
+                       <div class="falure">
+                        <h3> Sorry! </h3>
+                        <p>
+                            Registration not Completed. Please Try again...
+                        </p>
+                        
+                       </div> 
+                       <p class="submit">
+                        <input type="button" name="commit" value="Go to Registration" onclick="location.href='/app/standard/registration/registration.php'" >
+                       </p>
+                    <?php endif; ?>
+                    
+                </div>
+              </form>
+        </div>
+    </body>
+</html>
---
+++ temp/%%F4^F42^F42A6DC4%%default_home.html.php
@@ -0,0 +1,73 @@
+<?php /* Smarty version 2.6.6, created on 2014-01-04 08:49:32
+         compiled from E:/xampp/htdocs/stf/app_contents/standard/user_home/default_home.html */ ?>
+<html>
+
+     <head>
+        <title><?php echo $this->_tpl_vars['USER_FIRST']; ?>
+:</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <!-- stylesheets -->
+        <link rel="stylesheet" type="text/css" href="/app_contents/local/theme/admin/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="/app_contents/local/theme/admin/css/style.css" media="screen" />
+        <link id="color" rel="stylesheet" type="text/css" href="/app_contents/local/theme/admin/css/colors/blue.css" />
+
+        <!-- scripts (jquery) -->
+        <script src="/app_contents/local/theme/admin/scripts/jquery-1.4.2.min.js" type="text/javascript"></script>
+        <!--[if IE]><script language="javascript" type="text/javascript" src="/app_contents/local/theme/admin/scripts/excanvas.min.js"></script><![endif]-->
+        <script src="/app_contents/local/theme/admin/scripts/jquery-ui-1.8.custom.min.js" type="text/javascript"></script>
+        <script src="/app_contents/local/theme/admin/scripts/jquery.ui.selectmenu.js" type="text/javascript"></script>
+        
+        <script src="/app_contents/local/theme/admin/scripts/tiny_mce/tiny_mce.js" type="text/javascript"></script>
+        <script src="/app_contents/local/theme/admin/scripts/tiny_mce/jquery.tinymce.js" type="text/javascript"></script>
+
+        <!-- scripts (custom) -->
+        <script src="/app_contents/local/theme/admin/scripts/smooth.js" type="text/javascript"></script>
+        <script src="/app_contents/local/theme/admin/scripts/smooth.menu.js" type="text/javascript"></script>
+        
+        <script src="/app_contents/local/theme/admin/scripts/smooth.table.js" type="text/javascript"></script>
+        <script src="/app_contents/local/theme/admin/scripts/smooth.form.js" type="text/javascript"></script>
+        <script src="/app_contents/local/theme/admin/scripts/smooth.dialog.js" type="text/javascript"></script>
+
+        <script src="/app_contents/local/theme/admin/scripts/smooth.autocomplete.js" type="text/javascript"></script>
+        <script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/messages.js"></script>
+				<script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/common.js"></script>
+				<script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/sorttable.js"></script>
+				<script language="JavaScript" src="<?php echo $this->_tpl_vars['SYSTEM_COMMON_JAVASCRIPT_DIR']; ?>
+/CalendarPopup.js"></script>
+				<script language="JavaScript" src="<?php echo $this->_tpl_vars['REL_TEMPLATE_DIR']; ?>
+/<?php echo $this->_tpl_vars['SYSTEM_APP_PREFIX']; ?>
+.js"></script>
+        
+    </head>
+       
+        
+        <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => $this->_tpl_vars['USER_HEADER'], 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+        
+         <div id="content">
+         	
+         	   <div id="left"> 
+         	      <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => $this->_tpl_vars['USER_NAVIGATION'], 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+         	   </div>
+         	   <div id="right">
+         	      <?php echo $this->_tpl_vars['contents']; ?>
+
+         	   </div>   
+         	   <!-- footer -->
+         </div>
+         
+        <div id="footer">
+            <p>Copyright &copy; 2011 ASICT. All Rights Reserved.</p>
+        </div>
+        <!-- end footert -->
+<body>
